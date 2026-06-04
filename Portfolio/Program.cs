e387b1167f0a38d17f3103421246f41598c62c5f
Julio Gonzalez Garcia <julio.gonzalezgarcia0@gmail.com>
2026-06-03 22:31:44 -0700

    added new favico, moved headers aligned right, removed bg color

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -17,7 +17,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
